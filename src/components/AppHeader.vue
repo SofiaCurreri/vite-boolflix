70ede030ca7f66033bb9f29be412f7a2ffe49527
SofiaCurreri <sofiacurreri@gmail.com>
2023-03-06 15:53:23 +0100

    file struttura app + header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <header>
+    <div class="container my-3 d-flex justify-content-end">
+      <input type="text" @submit.prevent="" />
+      <button>Search</button>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped></style>
